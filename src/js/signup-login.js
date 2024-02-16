c6accf26d0271c89bb46e4ebfda2d098fd9260e1
LaNtZ445 <jacoblimparlantz@gmail.com>
2024-02-16 12:55:19 +0100

    :sparkles:

---
+++ src/js/signup-login.js
@@ -0,0 +1,30 @@
+/* function User(id, username, password, passwordIgen){
+    this.id = id,
+    this.username = username,
+    this.password = password,
+    this.passwordIgen = passwordIgen,
+    this.matches = function (searchFor) {
+        return (
+          this.username.toLowerCase().includes(searchFor) ||
+          this.password.toLowerCase().includes(searchFor) ||
+          this.passwordIgen.toLowerCase().includes(searchFor)
+        );
+    }
+}
+
+async function fetchUsers (){
+    return await (await fetch('http://localhost:3000/api/getAll'))
+}
+ */
+
+/* http://localhost:3000/login
+ */
+
+
+
+
+
+/* async function fetchPlayers(){
+    return await((await fetch('http://localhost:3000/api/hockeyPlayers/')).json())
+} 
+*/
