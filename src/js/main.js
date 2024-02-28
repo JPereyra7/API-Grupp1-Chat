bf382c41f7a64fb95dd3f8378492fdf332b96d20
Carolina Ekdal <Carolina.ekdal@gmail.com>
2024-02-28 12:47:23 +0100

    RenderMessages

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 const textMessage = document.getElementById("textMessage");
 const submitButton = document.getElementById("submitButton");
+const messagesContainer = document.getElementById("messagesContainer");
+const holderMessageBoxHolder = document.getElementById(
+  "holderMessageBoxHolder"
+);
+const holderActualMessage = document.getElementById("holderActualMessage");
+const senderMessageBoxHolder = document.getElementById(
+  "senderMessageBoxHolder"
+);
+const senderMessager = document.getElementById("senderMessager");
+const senderActualMessage = document.getElementById("senderActualMessage");
 
 submitButton.addEventListener("click", async (e) => {
   e.preventDefault();
@@ -25,6 +35,8 @@ submitButton.addEventListener("click", async (e) => {
 
     if (!response.ok) {
       console.error("Failed to send message");
+    } else {
+      await renderAllMessages();
     }
   } catch (err) {
     console.error(err, "server failed");
@@ -32,3 +44,48 @@ submitButton.addEventListener("click", async (e) => {
 });
 
 //skriv loop för rendera meddelandena
+async function renderAllMessages() {
+  const url = "http://localhost:3000/getAllMessages";
+  const method = "GET";
+
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    if (!response.ok) {
+      console.error("Failed to get message");
+    }
+    const allMessages = await response.json();
+    messagesContainer.innerHTML = "";
+    allMessages.forEach((message) => {
+      const messageBoxHolder = document.createElement("div");
+      const messageSenderContainer = document.createElement("div");
+      const messageSender = document.createElement("a");
+      const actualMessage = document.createElement("div");
+
+      actualMessage.textContent = `${message.messages}`;
+      messageSender.textContent = `${message.username}`;
+      console.log(`${message.messages}`);
+
+      messageBoxHolder.className = "message-box-holder";
+      messageSenderContainer.className = "message-sender";
+      actualMessage.className = "message-box message-partner";
+
+      messageSenderContainer.appendChild(messageSender);
+      messageBoxHolder.appendChild(messageSenderContainer);
+      messageBoxHolder.appendChild(actualMessage);
+      messagesContainer.appendChild(messageBoxHolder);
+    });
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  } catch (err) {
+    console.error(err, "server failed");
+  }
+}
+document.addEventListener("DOMContentLoaded", async () => {
+  await renderAllMessages();
+});
